c803f9b97e33d930b2d7035731471d7e9405543a
Oksana <73747282+latkova@users.noreply.github.com>
2020-11-08 20:02:45 +0200

    Update ChessGame.java

--- ChessGame.java
+++ ChessGame.java
@@ -14,19 +14,26 @@ public class ChessGame {
 
         String userPiece_1, innerPiece_1;
         String innerPiece_2;
+        String innerPiece_3;
         userPiece_1 = sc.next();
         innerPiece_1 = "king";
         innerPiece_2 = "bishop";
+        innerPiece_3 = "knight";
 
         if (userPiece_1.equals(innerPiece_1)){
 
             waysNumber = 4 * (64 - 4) + 6 * 4 * (64 - 6) + 6 * 6 * (64 - 9);
-            System.out.println(waysNumber + " ways  of setting up chess kings to avoid them of being captured");
-        }
+            System.out.println(waysNumber + " ways  of setting up chess " + innerPiece_1 + "s to avoid them of being captured");
+       }
 
         if (userPiece_1.equals(innerPiece_2)){
-            waysNumber = 28 * (64-8) + 20 * (64-10) + 12 * (64-12) + 4 * (64-14);
-            System.out.println(waysNumber + " ways  of setting up chess bishops to avoid them of being captured");
+            waysNumber = 28 * (64 - 8) + 20 * (64 - 10) + 12 * (64 - 12) + 4 * (64 - 14);
+            System.out.println(waysNumber + " ways  of setting up chess " + innerPiece_2 + "s to avoid them of being captured");
+        }
+
+        if (userPiece_1.equals(innerPiece_3)) {
+            waysNumber = 4 * (64 - 3) + 24 * (64 - 4) + 20 * (64 - 5) + 16 * (64 - 9);
+            System.out.println(waysNumber + " ways  of setting up chess " + innerPiece_3 + "s to avoid them of being captured");
         }
 
         else {
