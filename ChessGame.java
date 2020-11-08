dd6af994957f9010b2551cf22a17f7a5eab931c5
Oksana <73747282+latkova@users.noreply.github.com>
2020-11-08 20:40:31 +0200

    Update ChessGame.java

--- ChessGame.java
+++ ChessGame.java
@@ -1,24 +1,27 @@
 package com.company;
-// How many ways of setting up chess kings to avoid them of being captured?
+// How many ways of setting up chess pieces to avoid them of being captured?
 
 import java.util.Scanner;
 
 public class ChessGame {
 
     public static void main(String[] args) {
-      
+
         System.out.println("Enter chess piece (king, queen, rook, knight, bishop, or pawn): ");
-        
+
         Scanner sc = new Scanner(System.in);
         int waysNumber;
+
         String userPiece_1, innerPiece_1;
         String innerPiece_2;
         String innerPiece_3;
-        
+        String innerPiece_4;
+
         userPiece_1 = sc.next();
         innerPiece_1 = "king";
         innerPiece_2 = "bishop";
         innerPiece_3 = "knight";
+        innerPiece_4 = "queen";
 
         if (userPiece_1.equals(innerPiece_1)){
             waysNumber = 4 * (64 - 4) + 6 * 4 * (64 - 6) + 6 * 6 * (64 - 9);
@@ -33,6 +36,11 @@ public class ChessGame {
         if (userPiece_1.equals(innerPiece_3)) {
             waysNumber = 4 * (64 - 3) + 24 * (64 - 4) + 20 * (64 - 5) + 16 * (64 - 9);
             System.out.println(waysNumber + " ways  of setting up chess " + innerPiece_3 + "s to avoid them of being captured");
+        }
+
+        if (userPiece_1.equals(innerPiece_4)) {
+            waysNumber = 32 * (64 - 22) + 16 * (64 - 24) + 12 * (64 - 26) + 4 * (64 - 28);
+            System.out.println(waysNumber + " ways  of setting up chess " + innerPiece_4 + "s to avoid them of being captured");
         }
 
         else {
